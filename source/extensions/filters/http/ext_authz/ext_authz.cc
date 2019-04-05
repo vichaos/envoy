6c3c09f1f14e3225d6226cda8ccb8eea39bac274
Gabriel <gsagula@gmail.com>
2019-04-04 21:52:08 -0700

    fixed ambassador issue #1313
    Signed-off-by: Gabriel <gsagula@gmail.com>

--- source/extensions/filters/http/ext_authz/ext_authz.cc
+++ source/extensions/filters/http/ext_authz/ext_authz.cc
@@ -166,6 +166,8 @@ void Filter::onComplete(Filters::Common::ExtAuthz::ResponsePtr&& response) {
       if (header_to_modify) {
         ENVOY_STREAM_LOG(trace, " '{}':'{}'", *callbacks_, header.first.get(), header.second);
         Http::HeaderMapImpl::appendToHeader(header_to_modify->value(), header.second);
+      } else {
+        request_headers_->addCopy(header.first, header.second);
       }
     }
     cluster_->statsScope().counter("ext_authz.ok").inc();
